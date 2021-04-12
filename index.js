f08f92ecfaf536ea668097c70634b2e00c66b019
WalukaUI <cwaluka@yahoo.com>
2021-04-11 22:48:51 -0700

    started

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const fileMatch = require("file-match");
+
 // Add your code here
+
+let formData={
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+let submitData = {
+    method: "POST",
+    headers: {
+      "content-Type": "application/json",
+      "accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  fetch("http://localhost:3000/users", submitData)
