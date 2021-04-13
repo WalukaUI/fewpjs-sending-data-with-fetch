3257e0006212631e8d172510693e91fe420dc965
WalukaUI <cwaluka@yahoo.com>
2021-04-12 23:18:20 -0700

    started

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const fileMatch = require("file-match");
+//const fileMatch = require("file-match");
 
 // Add your code here
 
-let formData={
-    name: "Steve",
-    email: "steve@steve.com"
+
+
+function submitData(name,email){
+  
+let subData = {
+  method: "POST",
+  headers: {
+    "content-Type": "application/json",
+    "accept": "application/json"
+  },
+  body: JSON.stringify({
+    name,
+    email
+  })
+};
+  return fetch("http://localhost:3000/users",subData )
+
+.then(response => response.json())
+.then(object=> document.body.innerHTML=object['id'])
+.catch(error => document.body.innerHTML=error.message )
+
 }
-
-let submitData = {
-    method: "POST",
-    headers: {
-      "content-Type": "application/json",
-      "accept": "application/json"
-    },
-    body: JSON.stringify(formData)
-  };
-
-  fetch("http://localhost:3000/users", submitData)
+  
